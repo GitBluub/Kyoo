27e6ff1e8d9fa38a5e273fa7f926d45430d17806
GitBluub <louis.auzuret@gmail.com>
2022-11-12 21:58:22 +0900

    feat: rocket routes

--- src/Kyoo.Transcoder/src/main.rs
+++ src/Kyoo.Transcoder/src/main.rs
@@ -1,11 +1,16 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
+#[get("/direct/<_identifier>")]
+fn direct(_identifier: String) -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/transmux/<_identifier>")]
+fn transmux(_identifier: String) -> &'static str {
     "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/videos", routes![direct, transmux])
 }
